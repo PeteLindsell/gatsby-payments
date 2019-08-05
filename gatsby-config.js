eaa9ccf3bdfbf5b636ef6249d2b122d12fb0d3b4
Pete Lindsell <>
2019-08-05 09:42:32 +0100

    Async load stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,12 @@ module.exports = {
     `gatsby-plugin-flow`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-stripe`,
-    // {
-    //   resolve: `gatsby-plugin-stripe`,
-    //   options: {
-    //     async: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-stripe`,
+      options: {
+        async: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
